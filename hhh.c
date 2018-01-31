9e2cfa0e9004220815eb4582ee21e0410857da00
haripreethi <31505883+haripreethi@users.noreply.github.com>
2018-01-31 11:12:51 +0000

    Create hhh.c

---
+++ hhh.c
@@ -0,0 +1,16 @@
+    #include<stdio.h>
+    int main()
+    {
+    char a;
+    printf("enter the value");
+    scanf("%d",&x);
+    if((x>'a'&&x<'z')||(x>'A'&&x<'Z')
+    {
+    	printf("Alphabet");
+    }
+    else
+    {
+    	printf("Not Alphabet");
+     
+    }
+    }
