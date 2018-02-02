f3fff703ce0cb3dcf57082160b4b6eabe9a77950
haripreethi <31505883+haripreethi@users.noreply.github.com>
2018-02-02 10:59:43 +0000

    Create qqq.c

---
+++ qqq.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main()
+{
+int s;
+printf("enter the num=");
+scanf("%d",&s);
+{
+	printf("\nThe number %dis not prime:",s);
+}
+else
+{
+	printf("\nThe number %dis prime:",s)
+  getch();
+}
