709c74753444f2f525dc2d55f355747b9f315294
haripreethi <31505883+haripreethi@users.noreply.github.com>
2018-02-02 11:24:36 +0000

    Create kannan.c

---
+++ kannan.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int i,n,multiple=1;
+printf("Enter the value of n:");
+scanf("%d",&n);
+for(i=1;i<=5;i++)
+{
+multiple=n*i;
+printf("%d\t",multiple);
+}
+getch();
+}
