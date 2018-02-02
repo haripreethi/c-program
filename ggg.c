08e2cce96c261d5cc47621e0ab19f9b5b571f7a2
haripreethi <31505883+haripreethi@users.noreply.github.com>
2018-02-02 10:51:57 +0000

    Create ggg.c

---
+++ ggg.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include<math.h>
+ long power (inta, intb);
+ void main()
+{
+    int power=3, num=2;
+    long result;
+ 
+   printf("%d", a);
+    scanf("%d", &b);
+    printf("%d", a);
+    scanf("%d", &a);
+    result = power(num, pow);
+    printf("%d^%d is %ld", b, a, result);
+    return 0;
+}
+ 
+long power (int a, int b)
+{
+    if (a)
+    {
+        return (b * a(b ,a- 1));
+    }
+    getch()
+}
