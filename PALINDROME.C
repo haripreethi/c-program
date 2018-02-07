2459bd65014b60b3870064a7eadd0ecef09507e4
haripreethi <31505883+haripreethi@users.noreply.github.com>
2018-02-07 10:38:47 +0000

    Create PALINDROME.C

---
+++ PALINDROME.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c,d,e;
+	printf("enter the number");
+	scanf("%d",&a);
+	b=a/100;
+	c=(a-(1*100))/10;
+	d=(a-(1*100+c*10));
+	e=(d*100+c*10+b*1);
+	if(a==e)
+	printf("%d its palindro");
+	else
+	printf("it not palindro")
+  getch();
+	}
