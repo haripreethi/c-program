32ba25db13db2d4d890ca769082ba4c0928f6da9
haripreethi <31505883+haripreethi@users.noreply.github.com>
2018-02-21 10:57:56 +0000

    Create iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii.c

---
+++ iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+ 
+int main()
+{
+    char s[200];
+    int c=0,i;
+    scanf("%[^\n]s", s);
+    for (i = 0;s[i] != '\0';i++)
+    {
+        if (s[i] == ' ')
+            c++;    
+    }
+    printf("%d\n", c + 1);
+    return 0;
+}
